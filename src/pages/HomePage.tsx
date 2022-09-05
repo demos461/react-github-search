1695f860c8d1f7c3bfe4bacbb9dcbee6e2f72435
demos461 <iamdemos461@gmail.com>
2022-09-05 14:16:05 +0300

    create api

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import {useSearchUsersQuery} from "../store/github/github.api";
 
 export const HomePage = () => {
+    const {isLoading, isError, data} = useSearchUsersQuery('demos')
+
+
     return (
-        <div>
-            HomePage
+        <div className={'flex justify-center pt-10 mx-auto h-screen w-screen'}>
+            {isError && <p className={'text-center text-red-600'}>Something went wrong :(</p>}
         </div>
     );
 };
