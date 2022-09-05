245717a62c65cef155c894053cae860a879c79e8
demos461 <iamdemos461@gmail.com>
2022-09-05 12:50:33 +0300

    create routing and navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import {Route, Routes} from "react-router-dom";
+import {HomePage} from "./pages/HomePage";
+import {FavoritesPage} from "./pages/FavoritesPage";
+import {Navigation} from "./components/Navigation";
 
 function App() {
-  return (
-    <div>
-      Hello world!
-    </div>
-  );
+    return (
+        <>
+            <Navigation/>
+            <Routes>
+                <Route path={'/'} element={<HomePage/>}/>
+                <Route path={'/favorites'} element={<FavoritesPage/>}/>
+                <Route path={'*'} element={<div>404</div>}/>
+            </Routes>
+        </>
+    );
 }
 
 export default App;
